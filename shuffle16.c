801c216d952d026dc0a911a8068535f2c549dc0c
rtjk <47841774+rtjk@users.noreply.github.com>
2025-03-07 17:02:41 +0100

    test

--- shuffle16.c
+++ shuffle16.c
@@ -1,7 +1,7 @@
 #include <immintrin.h>
 #include <stdio.h>
 
-void print_m256i(__m256i var) {
+void print_m256i_8bit(__m256i var) {
     unsigned char vals[32];
     _mm256_storeu_si256((__m256i*)vals, var);
     printf("[ ");
@@ -21,9 +21,16 @@ void print_m256i_16bit(__m256i var) {
     printf("]\n");
 }
 
+/******************************************************************************/
+
+/* shuffle sixteen 16-bit integers packed into a 256-bit vector:
+   shuffle(a[], b[]) returns c[] where c[i]=a[b[i]] */
+
+/******************************************************************************/
+
 // OK
 // MaxMath/Runtime/XSE Core/Shuffles/Variable Permute.cs
-__m256i mm256_shuffle_epi16(__m256i a, __m256i b) {
+__m256i mm256_shuffle_epi16_A(__m256i a, __m256i b) {
     __m256i x1 = _mm256_setr_epi8(0, 0, 2, 2, 4, 4, 6, 6, 8, 8, 10, 10, 12, 12, 14, 14, 0, 0, 2, 2, 4, 4, 6, 6, 8, 8, 10, 10, 12, 12, 14, 14);
     __m256i x2 = _mm256_setr_epi8(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
     b = _mm256_adds_epu16(b, b);
@@ -33,9 +40,19 @@ __m256i mm256_shuffle_epi16(__m256i a, __m256i b) {
     return b;
 }
 
+/******************************************************************************/
 
+// OK
+__m256i mm256_shuffle_epi16_B(__m256i a, __m256i b) {
+    __m256i add_mask = _mm256_setr_epi8(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+    b = _mm256_adds_epu16(b, b);
+    b = _mm256_adds_epu16(b, _mm256_bslli_epi128(b, 1));
+    b = _mm256_adds_epu8(b, add_mask);
+    b = _mm256_shuffle_epi8(a, b);
+    return b;
+}
 
-
+/******************************************************************************/
 
 int main() {
 
@@ -50,7 +67,7 @@ int main() {
         0, 1, 2, 3, 4, 5, 6, 7
     );
 
-    __m256i result = mm256_shuffle_epi16(a, b);
+    __m256i result = mm256_shuffle_epi16_B(a, b);
 
     // Print the result
     printf("a:   ");
